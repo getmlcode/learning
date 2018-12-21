b448784f8f5f12dedcdb8d963ed4df10da346954
siddharthsvnit <siddharthsvnit@gmail.com>
2018-12-22 04:37:03 +0530

    added code to create frozen graph from within script

--- TensorFlow/SaveAndRstore/GraphDef/createFrozenGraph.py
+++ TensorFlow/SaveAndRstore/GraphDef/createFrozenGraph.py
@@ -1,5 +1,7 @@
+print('Importing Necessary Files')
 import numpy as np
 import tensorflow as tf
+from tensorflow.python.tools import freeze_graph
 import os
 import argparse
 
@@ -20,12 +22,15 @@ args = parser.parse_args()
 print('\nCheckpoint directory : ',args.dir)
 print('Model Name : {}'.format(args.model))
 
+
+
 b=tf.Variable(tf.zeros((10,)))
 W=tf.Variable(tf.random_uniform((4,10),-1,1))
 X=tf.placeholder(tf.float32,(1,4))
 h=tf.nn.relu(tf.matmul(X,W)+b)
 
 saver = tf.train.Saver()
+print('\nInitializing and saving graph checkpoints')
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     result = sess.run([h,X,W],{X:np.array([0.36948335, 0.13245803, 0.10355939, 0.9436994 ]).reshape(1,4)})
@@ -42,6 +47,22 @@ for node in graphDef.node:
 # creates test.pb and simpleModel.data that can be used as input to freeze_graph.py
 # to create frozenGraph.pb.
 # freeze_graph.py is in tensorflow/python/tools
-    
+
+print('\nSaving graph def')  
 tf.train.write_graph(graphDef, args.dir, "test.pb", False)
+print('\nFreezing Graph')
+
+
+freeze_graph.freeze_graph(input_graph=args.dir+'\\test.pb',
+                          input_saver='',
+                          input_checkpoint=args.dir+'\\simpleModel',
+                          output_graph=args.dir+'\\frozenGraph.pb',
+                          input_binary=True,
+                          output_node_names='Relu',
+                          initializer_nodes='',
+                          filename_tensor_name='',
+                          restore_op_name='',
+                          clear_devices=True
+                          )
+print('\nGraph Frozen')
 
